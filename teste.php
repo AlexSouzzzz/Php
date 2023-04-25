5e94fa0ed42e268bf295302fb47ae162ef8c132e
AlexSouzzzz <114254687+AlexSouzzzz@users.noreply.github.com>
2023-04-25 14:40:40 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,25 @@
+
+<?php
+
+$A = '17';
+$B = '8';
+if ($A %2 == 0) {
+echo" A é impar";
+}
+else {echo" A é par";
+}
+ 
+if ($B %2 == 0)
+{echo"<br> B é impar";
+}
+else{echo"<br> B é par";
+}
+
+if ($A>$B) {echo "<br>A é maior que B";
+}
+
+else{echo"<br> B é maior que A";
+}
+    ?>
+</body>
+</html>
